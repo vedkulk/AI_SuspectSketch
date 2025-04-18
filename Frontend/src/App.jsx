736c6b6ef3a8ea30b355002802193ffe892b90a3
vedkulk <kulkarnived3@gmail.com>
2025-04-19 00:10:40 +0530

    frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Header from './Header'
 
 const App = () => {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <>
+    <div className='min-w-screen min-h-screen bg-neutral-950'>
+      <Header/>
     </div>
+    </>
   )
 }
 
---
+++ Frontend/src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className='min-w-screen min-h-[50px] bg-gray-900 hover:text-blue-600 text-white text-left p-5 cursor-pointer'>
+        <h4>
+            SketchIntel
+        </h4>
+    </div>
+    </>
+  )
+}
+
+export default Header
